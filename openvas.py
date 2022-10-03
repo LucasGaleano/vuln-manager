fc63d5b2923e7f9160505a35aa25bac2fae04302
lucasgaleano <l.a.galeano2@gmail.com>
2022-10-03 16:44:06 -0300

    initial commit

---
+++ openvas.py
@@ -0,0 +1,83 @@
+import sys
+from datetime import datetime, timedelta
+import time
+import requests
+import json
+from openvasClient import OpenvasClient
+import configparser
+from loggingHelper import logger
+import pynetbox
+import ipaddress
+
+config = configparser.ConfigParser()
+config.read('openvas.conf')
+
+openvas_username = config['openvas']['username']
+openvas_password = config['openvas']['password']
+
+alerta_url = config['alerta']['url']
+alerta_api = config['alerta']['token']
+
+def main():
+
+    while True:
+        startTimeScan = time.time()
+        gmp = OpenvasClient(openvas_username, openvas_password)
+        gmp.authenticate()
+        logger.info('Starting Scan')
+        taskID = gmp.launch_scan(targetName=config['openvas']['targetName'] + ' ' + str(datetime.now()), scanConfigName=config['openvas']['scanConfigName'], hosts=get_netbox_ip())
+        gmp.wait_done(taskID, sleepTime=int(config['openvas']['checkScanInterval']))
+        results = gmp.get_results(taskID)
+
+        send_alerta(results)
+        logger.info(f'{len(results)} issues found') 
+        logger.info('Done')
+        
+        endTimeScan = time.time()
+        timeTaken = int(endTimeScan - startTimeScan)
+        sleepTime = int(config['openvas']['ScanInterval'])
+        sleepFor = max(sleepTime - timeTaken, 0)
+        logger.info(f"Scan finished, duration: {str(timedelta(seconds=timeTaken))}")
+        logger.info(f"Waiting {str(timedelta(seconds=sleepFor))} for the next scan")
+        time.sleep(sleepFor)
+
+
+def get_netbox_ip(publicIP=True):
+
+    nb = pynetbox.api(config['netbox']['url'], token=config['netbox']['token'])
+
+    reqIps = nb.ipam.ip_addresses.filter(tenant=config['netbox']['tenant'])
+
+    if publicIP:
+        ips = [str(ip).split('/')[0] for ip in reqIps if not ipaddress.ip_address(str(ip).split('/')[0]).is_private]
+    else:
+        ips = [str(ip).split('/')[0] for ip in reqIps if ipaddress.ip_address(str(ip).split('/')[0]).is_private]
+
+    logger.info(f'Scope: {", ".join(ips)}')
+    return ips
+
+
+
+
+def send_alerta(results):
+    headers = {'content-type': 'application/json'}
+    params = {'api-key': alerta_api}
+
+    data = {
+            "environment": "Security",
+            "event": f"{len(results)} issues found",
+            "origin": "External Penetration Test",
+            "resource": "External Penetration Test",
+            "service": [
+                       "External Penetration Test"
+                           ],
+            "severity": "critical",
+            "text": "Results from the external penetration test.",
+            "rawData" : '\n'.join(results)
+            }
+
+    response = requests.post(alerta_url, data=json.dumps(data), headers=headers, params=params)
+    logger.info(f'[+] Sending results to Alerta {response}')
+
+if __name__ == "__main__":
+    main()
---
+++ openvasClient.py
@@ -0,0 +1,91 @@
+
+from dataclasses import dataclass, field
+from gvm.connections import UnixSocketConnection
+from gvm.errors import GvmError
+from gvm.protocols.gmp import Gmp
+from gvm.transforms import EtreeCheckCommandTransform
+import sys
+from time import sleep
+from loggingHelper import logger
+
+@dataclass
+class OpenvasClient:
+    username: str
+    password: str
+    gmp: str = field(init=False)
+    con: str = field(init=False)
+
+    def authenticate(self):
+        path = '/run/gvmd/gvmd.sock'
+        connection = UnixSocketConnection(path=path)
+        transform = EtreeCheckCommandTransform()
+        self.con = Gmp(connection=connection, transform=transform)
+        self.con.connect()
+        self.gmp = self.con.determine_supported_gmp()
+        self.gmp.authenticate(self.username, self.password)
+        logger.info('Authentication Success')
+
+    def re_authenticate(self):
+        if not self.gmp.is_connected():
+            self.gmp.connect()
+            logger.info('Re-connecting to Openvas server')
+
+
+
+    def launch_scan(self, targetName, scanConfigName,hosts):
+        self.target(targetName, hosts)
+        return self.task(scanConfigName=scanConfigName, targetName=targetName)
+
+    def wait_done(self, taskID, sleepTime=100):
+        self.re_authenticate()
+        status, progress, results = self.get_task_info(taskID)
+        while status != 'Done':
+            self.re_authenticate()
+            logger.info(f'Status: {status} - Progress: {progress}% completed - results found: {results}')
+            sleep(sleepTime)
+            status, progress, results = self.get_task_info(taskID)
+
+    def target(self, name, hosts):   
+        try:
+            self.gmp.create_target(name=name, hosts=hosts, port_list_id=self.get_id(self.gmp.get_port_lists, "port_list", "nmap"))    
+
+        except GvmError as e:
+            logger.error(f'[-] Error: {e}')
+
+    def get_id(self, functionGet, elementName, name):
+        element = functionGet(filter_string=name)
+        return element.xpath(elementName)[0].get('id') 
+    
+
+    def task(self, scanConfigName, targetName=None):
+        try:
+            
+            task = self.gmp.create_task(name=targetName, config_id=self.get_id(self.gmp.get_scan_configs, "config", scanConfigName)
+                    , target_id=self.get_id(self.gmp.get_targets, "target", targetName)
+                    , scanner_id=self.get_id(self.gmp.get_scanners, "scanner", "OpenVAS default"))
+
+            self.gmp.start_task(task_id=task.get('id'))
+            
+            return task.get('id')
+
+        except GvmError as e:
+            logger.error(f'An error occurred {e}')
+
+    def get_task_info(self, taskID):
+        taskInfo = self.gmp.get_task(taskID).xpath('task')[0]
+        progress = taskInfo.find('progress').text
+        status = taskInfo.find('status').text
+        results = taskInfo.find('result_count').text
+        return status, progress, results
+
+    def get_results(self, taskID):
+        self.re_authenticate()
+        reportID = self.gmp.get_task(taskID).xpath('task')[0].xpath('last_report')[0].find('report').get('id')
+        report = self.gmp.get_report(reportID, ignore_pagination=True)
+        results = []
+        for result in report.xpath('report')[0].xpath('report')[0].xpath('results')[0]:
+            if float(result.find('severity').text) > 0.0 :
+                resultName = f"{result.find('host').text}:{result.find('port').text} - {result.find('name').text}"
+                results.append(resultName)
+                logger.info(f"[+] {resultName}")
+        return results
